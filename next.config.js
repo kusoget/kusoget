58eaa94679e57c6b0201cd853ec5f7d38bd765a6
kusoget <kusoget@outlook.jp>
2026-01-31 17:42:40 +0900

    feat: add bomb-box-game rewrite rules

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
+const BOMB_BOX_GAME_URL = process.env.BOMB_BOX_GAME_URL || 'https://<発行されたURL>';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      // Bomb Box Game へのプロキシ
+      {
+        source: '/bomb-box-game',
+        // 末尾の / は無し
+        destination: `${BOMB_BOX_GAME_URL}/bomb-box-game`,
+      },
+      {
+        source: '/bomb-box-game/:path*',
+        // 末尾の / は無し
+        destination: `${BOMB_BOX_GAME_URL}/bomb-box-game/:path*`,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
